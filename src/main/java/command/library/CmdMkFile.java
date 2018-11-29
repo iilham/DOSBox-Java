1463ac2bc8a623897d281e715b6ec2a50b7ee61a
Sugiri <girayable@gmail.com>
2018-11-29 13:51:23 +0700

    remove bug mkfile

--- src/main/java/command/library/CmdMkFile.java
+++ src/main/java/command/library/CmdMkFile.java
@@ -10,6 +10,8 @@ import interfaces.IDrive;
 import interfaces.IOutputter;
 import command.framework.Command;
 import filesystem.File;
+import filesystem.FileSystemItem;
+import java.util.ArrayList;
 
 class CmdMkFile extends Command {
 
@@ -19,9 +21,33 @@ class CmdMkFile extends Command {
 
 	@Override
 	public void execute(IOutputter outputter) {
-		String fileName = this.getParameterAt(0);
-		String fileContent = this.getParameterAt(1);
-		File newFile = new File(fileName, fileContent);
+            String fileName = this.getParameterAt(0);
+            String fileContent;
+            if(this.getParameterCount() <= 1){
+		fileContent = "";
+            } else{
+                fileContent = this.getParameterAt(1);
+            }
+            if (checkExists(fileName)){
+                System.out.println("File has already exists !");
+            } else {
+                File newFile = new File(fileName, fileContent);
 		this.getDrive().getCurrentDirectory().add(newFile);
+            }
+		
 	}
+        
+        private boolean checkExists (String filename){
+                ArrayList<FileSystemItem> fileExists = this.getDrive().getCurrentDirectory().getContent();
+                boolean status = false;
+                
+                for(int i =0; i<fileExists.size(); i++){
+                    if(!fileExists.get(i).getName().equals(filename) && status == false){
+                        status = false;
+                    } else{
+                        status = true;
+                    }
+                }
+                return status;
+        }
 }
